01ac37f4bfe8575102aa3a6b5e17234bc151af04
xuandong1994 <137134576+xuandong1994@users.noreply.github.com>
2023-09-14 08:59:45 +0700

    nop bai session2

---
+++ session2/src/nopbai/HinhChuNhat.java
@@ -0,0 +1,12 @@
+package nopbai;
+
+public class HinhChuNhat {
+    public static void main(String[] args) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ session2/src/nopbai/HinhTamGiacNguoc.java
@@ -0,0 +1,13 @@
+package nopbai;
+
+public class HinhTamGiacNguoc {
+    public static void main(String[] args) {
+        for (int i = 0; i < 6; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print(" * ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
